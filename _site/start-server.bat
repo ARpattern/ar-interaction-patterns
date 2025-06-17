9add59a98120bfae60966288c4c2a8d6f3314a7d
Joe <yuyu2152design@gmail.com>
2025-06-17 10:47:15 +0200

    Update

--- _site/start-server.bat
+++ _site/start-server.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d "C:\Users\user\Desktop\Document\GitHub_ARPattern_Web\ar-interaction-patterns"
+cd /d "C:\Users\user\Desktop\Document\PhD\GitHub_ARPattern_Web\ar-interaction-patterns"
 bundle exec jekyll serve --livereload
 pause
